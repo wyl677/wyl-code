adc796347d1de088f65cd390b172e5671d8a8664
wangyanlong <1475819449@qq.com>
2022-05-10 00:10:13 +0800

    注解1

--- java-base/src/com/wyl/reflect/autowire/Autowired.java
+++ java-base/src/com/wyl/reflect/autowire/Autowired.java
@@ -5,7 +5,7 @@ import java.lang.annotation.*;
 @Retention(RetentionPolicy.RUNTIME) //注解的保留位置
 @Target(ElementType.FIELD) //注解的作用目标
 @Inherited //说明子类可以继承父类中的该注解
-@Documented //说明该注解将被包含在javadoc中
+@Documented //说明该注解将被包含在javadoc中（描述使用javadoc工具为类生成帮助文档时是否保留其注解信息 javadoc -d doc MyDocumentedTest.java）
 /**
  * 注解其实就是代码里的特殊标记，它用于替代配置文件：传统方式通过配置文件告诉类如何运行，有了注解技术后，开发人员可以通过注解告诉类如何运行
  * 在Java技术里注解的典型应用是：可以通过反射技术得到类里面的注解，以觉得类怎么去运行。
