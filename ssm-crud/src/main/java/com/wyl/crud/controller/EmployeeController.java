31003d70997b876d1025e6f4451df962b6f1f977
wangyanlong <1475819449@qq.com>
2022-05-29 18:55:23 +0800

    ssm-crud提交：ajax展示员工列表

--- ssm-crud/src/main/java/com/wyl/crud/controller/EmployeeController.java
+++ ssm-crud/src/main/java/com/wyl/crud/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.wyl.crud.controller;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.wyl.crud.bean.Employee;
+import com.wyl.crud.bean.Msg;
 import com.wyl.crud.dao.EmployeeMapper;
 import com.wyl.crud.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -24,11 +26,26 @@ public class EmployeeController {
     @Autowired
     EmployeeService employeeService;
 
+    @RequestMapping(value = "/emps")
+    @ResponseBody //自动的将返回的对象转为json字符串
+    public Msg getEmpsWithJson(@RequestParam(value = "pn",defaultValue = "1") Integer pn){
+        // 这不是一个分页查询；
+        // 引入PageHelper分页插件
+        // 在查询之前只需要调用，传入页码，以及每页的大小
+        PageHelper.startPage(pn, 5);
+        // startPage后面紧跟的这个查询就是一个分页查询
+        List<Employee> emps = employeeService.getAll();
+        // 使用pageInfo包装查询后的结果，只需要将pageInfo交给页面就行了。
+        // 封装了详细的分页信息,包括有我们查询出来的数据，传入连续显示的页数
+        PageInfo page = new PageInfo(emps, 5);
+        return Msg.success().add("pageInfo",page);
+    }
+
     /**
      * 查询员工数据(分页查询)
      * @return
      */
-    @RequestMapping("/emps")
+//    @RequestMapping("/emps")
     public String getEmps(@RequestParam(value = "pn", defaultValue = "1") Integer pn, Model model) {
         // 这不是一个分页查询；
         // 引入PageHelper分页插件
---
+++ ssm-crud/src/main/webapp/index2.jsp
@@ -0,0 +1,23 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: wangyanlong
+  Date: 2022/5/17
+  Time: 下午11:33
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<jsp:forward page="/emps"></jsp:forward>
+<%--<html>
+<head>
+    <title>Title</title>
+    &lt;%&ndash;引入jquery&ndash;%&gt;
+    <script type="text/javascript" src="static/js/jquery-1.12.4.min.js"></script>
+    &lt;%&ndash;引入样式&ndash;%&gt;
+    <link href="static/bootstrap-3.4.1-dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="static/bootstrap-3.4.1-dist/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <button class="btn btn-success">按钮</button>
+
+</body>
+</html>--%>
